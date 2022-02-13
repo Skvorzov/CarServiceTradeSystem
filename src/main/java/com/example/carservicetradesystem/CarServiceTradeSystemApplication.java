e290ab767cf97568b00719c7c5a5e5f38fd04c5a
Skvortsov <skvorzov.sv@gmail.com>
2022-02-13 14:11:28 +0200

    Initial commit

---
+++ src/main/java/com/example/carservicetradesystem/CarServiceTradeSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.carservicetradesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarServiceTradeSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarServiceTradeSystemApplication.class, args);
+    }
+
+}
